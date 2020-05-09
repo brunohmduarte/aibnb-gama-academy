1c41f7e01b0c2bbb09e0655fa706188688f321b5
Bruno Duarte <bruno_ocz86@hotmail.com>
2020-05-09 14:21:37 -0300

    Paginação + Conclusão do layout

--- js/script.js
+++ js/script.js
@@ -1,87 +1,173 @@
-$(document).ready(function () {
 
-    const URL = "https://api.sheety.co/30b6e400-9023-4a15-8e6c-16aa4e3b1e72";
+const URL = "https://api.sheety.co/30b6e400-9023-4a15-8e6c-16aa4e3b1e72";
+var dados = [];
 
-    // Listando os feeds de locação da API.
-    $.get(URL, {}, (data) => {
+// Realizanod a requisição com a API.
+function sendRequestAPI(url, method = "GET", async = true) {
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function() {
+      if (this.readyState == 4 && this.status == 200) {
+        dados = JSON.parse(this.responseText)
+        infoHeader(dados.length)
+        paginate(1)
+      }
+    };
+    xhttp.open(method, url, async)
+    xhttp.send()
+}
 
-        let totalFeeds = data.length;
+/**
+ * Cálcula os itens que irão ser mostrados na página.
+ * 
+ * @param {array} items 
+ * @param {number} pageActual 
+ * @param {number} limitItems 
+ * @return Array result Contendo os Objetos a serem mostrados.
+ */
+function pagination(items, pageActual = 1, limitItems = 8) {
+    let result = [],
+        totalPage = Math.ceil(items.length / limitItems ),
+        count = ( pageActual * limitItems ) - limitItems,
+        delimiter = count + limitItems;
 
-        // Oculta a informação da quantidade de resultados da busca.
-        if (totalFeeds > 0) {
-            $(".header__amount").html(`Mais de ${totalFeeds} acomodações para hospedagem.`);
-        }
+    if (pageActual <= totalPage){
+        for(let i=count; i<delimiter; i++){
+            if(items[i] != null){
+                result.push(items[i]);
+            }
+            count++;
+         }
+    }
+    return result;
+};
 
-        // Lista os feeds.
-        data.forEach(element => {
-            $(".content").append(`
-                <div class="col-sm-12 col-md-6 col-lg-4 col-xl-3">
-                    <div class="card m-auto border-0">
-                        <div class="content__card-image">
-                            <img src="${element.photo}" class="card-img-top" alt="${element.property_type}">
-                        </div>
-                        <div class="card-body">
-                            <h5 class="card-title">${element.property_type}</h5>
-                            <p class="card-text content__card-description">${element.name}</p>
-                            <p class="card-text content__card-price"><strong>R$ ${element.price}</strong><span>,00</span>/diária</p>
-                            <a href="#">Reservar</a>
-                        </div>
+/**
+ * Imprime o HTML na página.
+ * 
+ * @param Array paginate Array de objetos contendo os elementos a serem mostrados.
+ * @return void
+ */
+function listCards(paginate = []) { 
+    let html = "";
+    paginate.forEach(element => {
+        html += `
+            <div class="col-sm-12 col-md-6 col-lg-4 col-xl-3">
+                <div class="card m-auto border-0">
+                    <div class="content__card-image">
+                        <img src="${element.photo}" class="card-img-top" alt="${element.property_type}">
+                    </div>
+                    <div class="card-body">
+                        <h5 class="card-title">${element.property_type}</h5>
+                        <p class="card-text content__card-description">${element.name}</p>
+                        <p class="card-text content__card-price"><strong>R$ ${format_number(element.price, 2, "", ".")}</strong><span>,00</span>/diária</p>
+                        <a href="#">Reservar</a>
                     </div>
                 </div>
-            `);
-        });
+            </div>
+        `;
     });
-    
-    // TODO - Verifica junto ao slack para ver se o autor compartilhou o codigo dele para comparar e ver o por que não esta funfando.
+    document.getElementsByClassName("content")[0].innerHTML = html;
+}
 
-    // const PAGING_OPTIONS = { 
-    //     totalCount: 0,      // O conteúdo total contar
-    //     itemPerPage: 12,    // Quantos itens serão mostrados por página
-    //     initialPage: 1,     // inicial Página (Podemos definir este parâmetro através da detecção de página atual da string de consulta)
-    //     get totalPage() {   // Calculando a contagem total de páginas retornando Math.ceil (this.totalCount / this.itemPerPage) // Estou usando Math.ceil para teto
-    //         return Math.ceil(this.totalCount / this.itemPerPage)
-    //     }, 
-    //     local: ".paging",   // É o seletor a ser renderizado URL do componente de paginação : "/ api / contents /? id = 5a5e47ec7152d929bc6d4280 & p =" // O endereço da URL que será solicitado após a alteração de cada página. 
-    // }
+/**
+ * Imprime o HTML dos links da paginação na página.
+ * 
+ * @param {array} items Array de objetos contendo os elementos a serem mostrados.
+ * @param {number} pageActual Index da paginação atual.
+ * @return void
+ */
+function paginateLink(items, pageActual = 1) {
+    let html = "",
+        totalLink = Math.ceil(items.length / 8),
+        disabledBack = pageActual == 1 ? "disabled" : "",
+        disabledNext = pageActual == totalLink ? "disabled" : "",
+        indexProgressBack = pageActual != 1 ? pageActual - 1 : 1;
+        indexProgressNext = pageActual != totalLink ? pageActual + 1 : pageActual;
 
-    // const PAGING = {
-    //     append,
-    //     bindEventHandlers,
-    //     createButton,
-    //     createPaging,
-    //     createPaging,
-    //     getContent,
-    //     getData,
-    //     handleItemClick,
-    //     handleNextClick,
-    //     handlePrevClick,
-    //     init
-    // }
+    // Link back
+    html += `
+        <li class="paginate__item ${disabledBack}">
+            <a class="paginate__link" href="javascript:paginate(${indexProgressBack})">
+                <i class="fa fa-chevron-left" aria-hidden="true"></i>
+            </a>
+        </li>
+    `;
 
-    // init: options => {
-    //     PAGING.createPaging(options, () => {
-    //         PAGING.bindEventHandlers(options);
-    //         PAGING.getContent(options.URL + options.initialPage);
-    //     });
-    // };
+    // Link pagination
+    for (let i=1; i<=totalLink; i++) {
+        let active = pageActual == i ? "active" : "";
+        html += `
+            <li class="paginate__item ${active}">
+                <a class="paginate__link" href="javascript:paginate(${i})">${i}</a>
+            </li>
+        `;
+    }
 
-    // createButton: ({direction, isActive = true, text, isCurrent, index}) => {
-    //     const DIRECTION_CLASS = direction === "back" ? "paging__btn--nav paging__btn--back" 
-    //                                                  : direction === "next" ? "paging__btn--nav paging__btn--back" 
-    //                                                                        : "",
-    //           BUTTON_TEXT = direction === "back" ? "&laquo;" : direction === "next" ? "&raquo;" : text,
-    //           ACTIVE_CLASS = !isActive ? "paging__btn--disabled" : "",
-    //           CURRENT_CLASS = isCurrent ? "paging__btn--current" : "",
-    //           MODIFIERS = [ACTIVE_CLASS, CURRENT_CLASS, DIRECTION_CLASS],
-    //           BUTTON_HTML = `<button data-page-index="${index}" class="paging__btn ${MODIFIERS.join(" ")}">${BUTTON_TEXT}</button>`;
-    
-    //     return BUTTON_HTML;
-    // };
+    // Link next
+    html += `
+        <li class="paginate__item ${disabledNext}">
+            <a class="paginate__link" href="javascript:paginate(${indexProgressNext})">
+                <i class="fa fa-chevron-right" aria-hidden="true"></i>
+            </a>
+        </li>
+    `;
+    document.getElementsByClassName("paginate__list")[0].innerHTML = html;
+}
 
-    // PAGING.init(PAGING_OPTIONS);
+/**
+ * Mostra a quantidade de registro resultantes da busca.
+ * 
+ * @param {number} items número de locações.
+ * @return void
+ */
+function infoHeader(items) {    
+    document.getElementsByClassName("header__amount")[0].innerHTML = `Mais de ${items} acomodações para hospedagem.`;
+}
 
+/**
+ * Limpa o conteúdo dos cards.
+ * 
+ * @return void
+ */
+function clearBoxContent() {
+    document.getElementsByClassName("content")[0].innerHTML = "";
+}
 
-    // const myRequest = new Request(URL, {method: "GET"});
-    // console.log(myRequest)
-        
-});
+/**
+ * Limpa o conteúdo dos links da páginação.
+ * 
+ * @return void
+ */
+function clearBoxLinkPaginate() {
+    document.getElementsByClassName("paginate__list")[0].innerHTML = "";
+}
+
+/**
+ * Responsável por executar à paginação.
+ * 
+ * @return void
+ */
+function paginate(pageCurrent) { 
+    clearBoxContent();
+    listCards(pagination(dados, pageCurrent));
+
+    clearBoxLinkPaginate();
+    paginateLink(dados, pageCurrent);
+}
+
+function format_number(number, decPlaces, decSep, thouSep) {
+    decPlaces = isNaN(decPlaces = Math.abs(decPlaces)) ? 2 : decPlaces,
+    decSep = typeof decSep === "undefined" ? "." : decSep;
+    thouSep = typeof thouSep === "undefined" ? "," : thouSep;
+    var sign = number < 0 ? "-" : "";
+    var i = String(parseInt(number = Math.abs(Number(number) || 0).toFixed(decPlaces)));
+    var j = (j = i.length) > 3 ? j % 3 : 0;
+
+    return sign +
+        (j ? i.substr(0, j) + thouSep : "") +
+        i.substr(j).replace(/(\decSep{3})(?=\decSep)/g, "$1" + thouSep);
+        // (decPlaces ? decSep + Math.abs(number - i).toFixed(decPlaces).slice(2) : "");
+}
+
+// Carregando as informações da API.
+sendRequestAPI(URL);
